5bded31b4c178aa5683d1aaf16e6af95ad0db24e
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-10-14 13:58:47 -0700

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Chrishnika Paul on 10/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
