05dbf5f9f96dc670a20cbcc97ad8c0679ec0e7ed
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-10-23 15:13:09 -0700

    Commented and organized GameViewController

--- Lab3/GameViewController.swift
+++ Lab3/GameViewController.swift
@@ -11,14 +11,19 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
-    var junkBudget:Int!
+    //MARK: - Variables
+    var junkBudget:Int!     //Used to store game currency. Passed from calling vc.
     
+    //MARK: - View functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //setup game scene
         let scene = GameScene(size: view.bounds.size)
+        
+        //Updates the game with currency from exceeding step goals
         scene.updateJunkBudgetTo(val: junkBudget)
+        
         let skView = view as! SKView // the view in storyboard must be an SKView
         skView.showsFPS = true // show some debugging of the FPS
         skView.showsNodeCount = true // show how many active objects are in the scene
@@ -31,16 +36,4 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden : Bool {
         return true
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
